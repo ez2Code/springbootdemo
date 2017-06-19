34091cbc2c1cacfa3123c5e54a6c58eb28f5d2fa
li_weia <li_weia@ctrip.com>
2017-06-19 15:40:28 +0800

    .

--- src/main/java/com/ctrip/controller/HelloController.java
+++ src/main/java/com/ctrip/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.ctrip.controller;
 
-import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by li_weia on 2017/6/16.
@@ -12,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     @ResponseBody
-    JSONObject home() {
-        JSONObject obj = new JSONObject();
-        obj.put("name","Jack");
-        return obj;
+    Map home() {
+        Map<String, String> result= new HashMap<>();
+        result.put("name","Jack");
+        return result;
     }
 }
