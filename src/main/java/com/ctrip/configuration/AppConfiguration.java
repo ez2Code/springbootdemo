2377d1c5f1febfd53692198b5b1699af7df8269c
li_weia <li_weia@ctrip.com>
2017-06-22 15:56:49 +0800

    .

--- src/main/java/com/ctrip/configuration/AppConfiguration.java
+++ src/main/java/com/ctrip/configuration/AppConfiguration.java
@@ -1,4 +1,4 @@
-package com.ctrip.config;
+package com.ctrip.configuration;
 
 import com.ctrip.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by li_weia on 2017/6/21.
  */
 @Configuration
-public class AppConfigurer extends WebMvcConfigurerAdapter {
+public class AppConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**");
